c86539a413aa110fc2092fca9b855f09158761d5
riazm13 <riazm13@uni.coventry.ac.uk>
2021-12-10 13:59:27 +0000

    Starting JS

---
+++ Week 10/Starting_JS/index.js
@@ -0,0 +1,40 @@
+"numbers"
+//Addition
+5646515616 + 6546541561
+
+//Subtraction
+90 - 1
+
+//Multiplication
+1111111111 * 7
+
+//Division
+400 / 25
+
+//Modulo!! //for remainders
+27 % 2
+
+//this is a Comment in JS
+
+// PEMDAS
+
+/*  parenthases
+    Exponents
+    Multiplication
+    Division
+    Addition
+    Subtraction
+*/
+
+// 3 + 1 * 9 = 12 1 times 9 first then add the 3 is the order it will work
+
+//NaN Not a Number
+
+//0/0 NAN
+// classed as a number but isnt acctually one
+
+4 + 3 * 4 / 2 = 10
+
+    (13 * 5) ** 2 =  9
+
+200 + 0 / 0 = NaN

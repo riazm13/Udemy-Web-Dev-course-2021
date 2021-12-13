fbb3491aa3eb392924e12ea43c6e3594d0b766b4
riazm13 <riazm13@uni.coventry.ac.uk>
2021-12-13 13:36:18 +0000

    Reminding myself about variables

---
+++ Week 11/Monday/Variables.js
@@ -0,0 +1,32 @@
+// a way to store a value so we can use it later and refer back to it 
+// the value can be used to do stuff and changed later 
+// let someName = value;
+let year = 1985;
+let numHens = 5;
+let numRoosters = 1;
+let totalChickens = numHens + numRoosters;
+
+let score = 0;
+
+score = score + 5;
+
+let numLives = 9;
+
+//numLives = numLives += 1;
+//numLives ++
+// Both increment by 1
+
+const feetInMile = 5280;
+// const cannot be changed at all
+const luckyNum = 7;
+
+// Booleans TRUE or FALSE
+// they store yes or no values
+
+let isGameActive = true;
+isGameActive = false;
+
+// its not a fixed type so you can change it too a num or str
+// Although doing this is not very common
+
+isGameActive = 426525;

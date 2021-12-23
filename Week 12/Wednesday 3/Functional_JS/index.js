434ce095c8f47665c9cae2562876614a66134ec7
riazm13 <riazm13@uni.coventry.ac.uk>
2021-12-23 11:55:56 +0000

    practice with functions as udmy dont work

---
+++ Week 12/Wednesday 3/Functional_JS/index.js
@@ -0,0 +1,13 @@
+const a = 20
+const b = 2
+// Function is called, return value will end up in x
+
+const part1 = function myFunction(a, b) {
+    return a * b;             // Function returns the product of a and b
+}
+
+console.log(part1(4, 4));
+console.log(part1(4, 2));
+ // inputs need to be added to the function after the fact so that the logic can run
+// function can be called on as many times as needed and also have diff values run through it
+// logic will allways stay the same

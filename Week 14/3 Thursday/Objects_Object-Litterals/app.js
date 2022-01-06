d3eb8e0464f506a540885394b136b8be053119d3
riazm13 <riazm13@uni.coventry.ac.uk>
2022-01-06 14:35:19 +0000

    Starting object litterals

---
+++ Week 14/3 Thursday/Objects_Object-Litterals/app.js
@@ -0,0 +1,42 @@
+// const dataCollected = {
+//     totalSteps: 308727,
+//     totalMiles: 211.7,
+//     avgCalorieBurn: 5755,
+//     workoutsThisWeek: "5 of 7",
+//     avgGoodSleep: "2:13"
+// };
+// There is a Key and a value 
+// Both are needed
+
+
+// Going to be learning about objects
+// Talking about the information inside the {} "Curly Braces"
+
+const person = {
+    firstName: "Mick",
+    lastName: "Jagger"
+}
+
+let comment = {
+    userName: "SlimeLad246",
+    downVotes: 19,
+    upVotes: 324,
+    netScore: 195,
+    commentText: "Tastes like chicken",
+    tags: ["#hilarious", "#Funny", "Silly"],
+    isGuilded: false
+};
+
+// ============ SELF TEST ==============
+// Your code goes in here:
+
+const product = {
+    name: "Gummy Bears",
+    inStock: true,
+    price: 1.99,
+    flavors: ["grape", "apple", "cherry"]
+
+};
+
+  //=============================================
+

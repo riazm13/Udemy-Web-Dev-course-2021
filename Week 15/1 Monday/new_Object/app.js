5ecceacc27013eeeeadeed6f95528a3ec2cbffc1
riazm13 <riazm13@uni.coventry.ac.uk>
2022-01-13 12:57:03 +0000

    Nesting objects in variables and arrays

---
+++ Week 15/1 Monday/new_Object/app.js
@@ -0,0 +1,16 @@
+const midterms = {
+    daniell: 96,
+    thomas: 76
+};
+
+midterms.thomas = 80;
+midterms.thomas = "C+";
+midterms.ezra = "A+"
+midterms['antonio'] = "A";
+
+const comments = [
+    { userName: 'Tammy', text: 'lololololol', votes: 9 },
+    { userName: 'fish', text: 'I am a Fish', votes: 19 }
+];
+
+console.log(comments[1].text);

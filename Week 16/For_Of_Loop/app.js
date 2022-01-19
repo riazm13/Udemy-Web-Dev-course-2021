8133c6b9e0b967f5574a1210a61497d95952cad9
riazm13 <riazm13@uni.coventry.ac.uk>
2022-01-19 14:06:51 +0000

    for of loop

---
+++ Week 16/For_Of_Loop/app.js
@@ -0,0 +1,38 @@
+// const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer', 'gunners'];
+
+// for (let i = 0; i < subreddits.length; i++) {
+//     console.log(`These are the subreddits: ${subreddits[i]}`);
+// }
+
+// for (let sub of subreddits) {
+//     console.log(sub);
+// }
+
+// const seatingChart = [
+//     ['Kristen', 'Eren', 'Namita'],
+//     ['Geffory', 'Juan', 'Antonio', 'Kevin'],
+//     ['Yuma', 'Sakura', 'Jack', 'Erika']
+// ]
+
+// for (let row of seatingChart) {
+//     for (let student of row) {
+//         console.log(student);
+//     }
+
+// }
+
+// for (let char of "hello World") {
+//     console.log(char);
+// }
+//============ SELF TEST =============
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]; //DON'T CHANGE THIS LINE PLEASE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+
+for (const square of numbers) {
+    // console.log(square);
+    let newNum = square * square;
+    console.log(newNum);
+}
+
+//============ SELF TEST =============

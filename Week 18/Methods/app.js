522ea5dd9001e9df8a75ef6b40fb18bd88fb8b10
riazm13 <riazm13@uni.coventry.ac.uk>
2022-01-31 13:00:26 +0000

    methods

---
+++ Week 18/Methods/app.js
@@ -0,0 +1,11 @@
+const math = {
+    multiply: function (x, y) {
+        return x * y;
+    },
+    divide: function (x, y) {
+        return x / y;
+    },
+    square: function (x) {
+        return x * x;
+    }
+};

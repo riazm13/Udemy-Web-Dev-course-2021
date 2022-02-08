62e15e2690617af9213bfcd349dead521f3b7b80
riazm13 <riazm13@uni.coventry.ac.uk>
2022-02-08 12:54:09 +0000

    setTimeout and arrow funcs

---
+++ Week 19/Arrow-Functions/app.js
@@ -0,0 +1,54 @@
+const square = (x) => {
+    return x * x;
+}
+
+const sum = (x, y) => {
+    return x + y;
+}
+
+// const rollDie = () => {
+//     return Math.floor(Math.random() * 6) + 1;
+// }
+
+const rollDie = () => (
+    Math.floor(Math.random() * 6) + 1
+)
+
+//=========== SELF TEST ==============
+
+const greet = (name) => {
+    return `Hey ${name}!`;
+}
+
+//=========== SELF TEST ==============
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+];
+
+// const newMovies = movies.map(function (movie) {
+//     return `${movie.title} - ${movie.score / 10}`;
+// })
+
+const newMovies = movies.map(movie => {
+    return `${movie.title} - ${movie.score / 10}`
+})
+
+setTimeout(() => {
+    console.log("Hello");
+}, 3000)

df600c5692ffe320f96f82359608cea67a6db96e
riazm13 <riazm13@uni.coventry.ac.uk>
2022-02-18 11:14:41 +0000

    Friday

--- Week 20/Rest/app.js
+++ Week 20/Rest/app.js
@@ -6,3 +6,12 @@ function sum(...nums) {
     return nums.reduce((total, el) => total + el)
 
 }
+
+
+function raceResults(gold, silver, ...everyOneElse) {
+    console.log(`First place is ${gold}!`);
+    console.log(`Second place is ${silver}`);
+    console.log(`Participation trophies go to ${everyOneElse}`);
+}
+
+raceResults('Tyrone', 'Lucy', 'Sam', 'Gibby', 'Wraith', 'Alex');

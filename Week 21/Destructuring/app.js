92e319c24682b31ddd3d0ad8ffbf19185af3e0b3
riazm13 <riazm13@uni.coventry.ac.uk>
2022-02-18 13:03:39 +0000

    destruct

---
+++ Week 21/Destructuring/app.js
@@ -0,0 +1,6 @@
+const scores = [3465346, 346346, 6589758647, 3465357, 6586574567, 4542567, 68679757, 4575745];
+
+const highScore = scores[0];
+const secondHighScore = scores[1];
+
+const [gold, silver, bronze, ...allOtherScores] = scores;

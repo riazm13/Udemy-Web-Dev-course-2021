d9dfce56a707d35b9a9d66b509022696d173361f
riazm13 <riazm13@uni.coventry.ac.uk>
2022-02-22 12:50:55 +0000

    starting DOM

--- Week 21/Destructuring/app.js
+++ Week 21/Destructuring/app.js
@@ -4,3 +4,50 @@ const highScore = scores[0];
 const secondHighScore = scores[1];
 
 const [gold, silver, bronze, ...allOtherScores] = scores;
+
+//=======================================
+
+const user = {
+    email: 'harvey@gmail.com',
+    password: 'HelloWorld123',
+    firstName: 'Harvey',
+    lastName: 'Milk',
+    born: 1930,
+    died: 1978,
+    bio: 'Harvey Bernard Milk was an american politiian ...',
+    city: 'Sam Francisco',
+    state: 'California'
+}
+
+const user2 = {
+    email: 'stacey@gmail.com',
+    firstName: 'Stacey',
+    lastName: 'Gonzalez',
+    born: 1987,
+    city: 'Tulsa',
+    state: 'Oklahoma'
+}
+
+// const firstName = user.firstName;
+// const lastName = user.lastName;
+// const email = user.email;
+
+// const { email, firstName, lastName, city, bio } = user;
+
+// const { born: birthYear, died: yearOfDeath } = user;
+
+const { city, state, died = 'N/A' } = user2;
+
+
+// function fullName(user) {
+//     return `${user.firstName}${user.lastName}`
+// }
+
+// function fullName(user) {
+//     const { firstName, lastName } = user;
+//     return `${firstName} ${lastName}`
+// }
+
+function fullName({ firstName, lastName }) {
+    return `${firstName} ${lastName}`
+}

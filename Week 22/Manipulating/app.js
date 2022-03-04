1d80d8055cadaf1868ee23c639db89a27dab0527
riazm13 <riazm13@uni.coventry.ac.uk>
2022-03-04 10:36:34 +0000

    Week 22

---
+++ Week 22/Manipulating/app.js
@@ -0,0 +1,58 @@
+const allLinks = document.querySelectorAll('a');
+
+// for (let link of allLinks) {
+//     link.innerText = 'I AM A LINK!!!!'
+// }
+
+
+for (let link of allLinks) {
+    link.style.color = 'rgb(0, 108, 134)';
+    link.style.textDecorationColor = 'magenta';
+    link.style.textDecorationStyle = 'wavy'
+}
+
+
+// ================ SELF TEST ==================
+
+// <!DOCTYPE html>
+
+// <head>
+//     <title>Pickles</title>
+//     <!--LEAVE THESE LINES ALONE, PLEASE! THEY MAKE THE LIVE PREVIEW WORK!-->
+//     <script src="node_modules/babel-polyfill/dist/polyfill.js" type="text/javascript"> </script>
+//     <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+
+// </head>
+
+// <body>
+//     <!--PLEASE LEAVE THIS LINE ALONE! MAKE YOUR CHANGES USING JAVASCRIPT!!-->
+//     <h1>Pickles Are <span>Delicious</span></h1>
+// </body>
+
+// </html>
+
+// ================ SELF TEST ==================
+// const allSpans = document.querySelectorAll('span');
+// for (let span of allSpans) {
+//     span.innerText = 'Disgusting'
+// }
+// ================ SELF TEST ==================
+
+// < !--DONT CHANGE ANYTHING IN THIS FILE, PLEASE!-- >
+//     Egg and Chicken
+
+
+//         < h4 > Which Came First ?</h4 >
+//             <img src="https://devsprouthosting.com/images/egg.jpg" width="200px">
+//                 <!-- image source: unsplash.com -->
+
+
+// ================ SELF TEST ==================
+
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+{/* const image1 = document.querySelector('img'); 
+image1.src = 'https://devsprouthosting.com/images/chicken.jpg';
+image1.alt = 'chicken'; */}
+
+
+// ================ SELF TEST ==================
